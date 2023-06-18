3acf09b94bae946be785a429a0a9a4e08719c4df
Hariom Singh <hariomhks1997@gmail.com>
2023-06-18 22:34:42 +0530

    Understanding JSX _ Complete React Course in Hindi #4 (720p60)

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+let name = "Harry";
+function App() {
+  return (
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">
+        <h1>Hello {name}</h1>
+
+        <p>
+          fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
