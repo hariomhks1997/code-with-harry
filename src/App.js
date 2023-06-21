88d56925b7f2e1e12a57c699136200fae33099a6
Hariom Singh <hariomhks1997@gmail.com>
2023-06-21 10:54:36 +0530

    Adding + Auto Dismissing Alert Messages _ Complete React Course in Hindi #13 (720p60)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Module from "./Module";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
 
 function App() {
   const [mode, setmode]=useState("light")
@@ -12,19 +13,32 @@ function App() {
     {
       setmode("dark")
       document.body.style.backgroundColor="#042743";
+      showAlert("Dark Mode has been enabled","Sucess")
     }
     else{
       setmode("light")
       document.body.style.backgroundColor="white";
+      showAlert("Light Mode has been enabled","Sucess")
     }
+  }
+  const [alert, setalert]=useState("");
+  const showAlert=(message,type)=>{
+ setalert({
+  msg:message,
+  type:type
+ })
+ setTimeout(()=>{
+  setalert("")
+ },2000)
   }
   return (
     <>
       <Navbar title="TextUtils" aboutText="About" mode={mode} togglemode={togglemode}></Navbar>
       
       {/* <Navbar></Navbar> */}
+      <Alert alert={alert}></Alert>
       <div className="container my-3"> 
-      <TextForm heading="Enter The Text To Analyze Below" mode={mode}></TextForm>
+      <TextForm  heading="Enter The Text To Analyze Below" mode={mode} showAlert={showAlert}></TextForm>
       <About></About>
       <Module></Module>
       </div>
